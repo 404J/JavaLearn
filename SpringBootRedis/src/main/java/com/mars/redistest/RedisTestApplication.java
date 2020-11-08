18bca7e654b691703ccc3878fe1b8afd1c5025d3
404J <j1491361626@gmail.com>
2020-11-08 21:47:08 +0800

    add

--- SpringBootRedis/src/main/java/com/mars/redistest/RedisTestApplication.java
+++ SpringBootRedis/src/main/java/com/mars/redistest/RedisTestApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class RedistestApplication {
+public class RedisTestApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(RedistestApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(RedisTestApplication.class, args);
         TestRedis redis = ctx.getBean(TestRedis.class);
         redis.testRedis();
     }
--- SpringBootRedis/src/main/java/com/mars/redistest/TestRedis.java
+++ SpringBootRedis/src/main/java/com/mars/redistest/TestRedis.java
@@ -5,6 +5,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Component
 public class TestRedis {
 
@@ -15,11 +19,32 @@ public class TestRedis {
     StringRedisTemplate stringRedisTemplate;
 
     public void testRedis() {
-        redisTemplate.opsForValue().set("k1", "hello");
-        System.out.println(redisTemplate.opsForValue().get("k1"));
+        stringRedisTemplate.opsForValue().set("k1", "hello");
+        System.out.println(stringRedisTemplate.opsForValue().get("k1"));
 
         stringRedisTemplate.opsForValue().set("k2", "world");
         System.out.println(stringRedisTemplate.opsForValue().get("k2"));
+
+        ArrayList<String> keys = new ArrayList<>();
+        keys.add("k1");
+        keys.add("k2");
+
+        List<String> values = stringRedisTemplate.opsForValue().multiGet(keys);
+        System.out.println(values);
+
+        stringRedisTemplate.opsForValue().append("k1", " mars");
+        System.out.println(stringRedisTemplate.opsForValue().get("k1"));
+
+        System.out.println(stringRedisTemplate.opsForValue().get("k1", 1 , 2));
+
+        stringRedisTemplate.opsForValue().set("k3", "5");
+        System.out.println(stringRedisTemplate.opsForValue().increment("k3"));
+
+        stringRedisTemplate.opsForValue().setBit("k4", 2, false);
+        System.out.println(stringRedisTemplate.opsForValue().getBit("k4", 2));
+
+        Set<String> allKeys = stringRedisTemplate.keys("*");
+        System.out.println(allKeys);
 
     }
 }

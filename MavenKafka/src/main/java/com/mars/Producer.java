bbfbcb79c91241e93988e6d36483048a2334dd44
404J <j1491361626@gmail.com>
2021-01-17 22:15:58 +0800

    add

--- MavenKafka/src/main/java/com/mars/Producer.java
+++ MavenKafka/src/main/java/com/mars/Producer.java
@@ -12,16 +12,16 @@ import java.util.concurrent.Future;
 
 public class Producer {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        String topic = "maven-kafka";
+        String topic = "test";
 
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "0");
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(properties);
 
-//        while (true) {
+        while (true) {
             for (int i = 0; i < 3; i++) { // 区分消息的内容
                 for (int j = 0; j < 3; j++) { // 区分消息的类别，根据 key 区分，使其发送到同一个 Partition
                     ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, "msg-" + j, "val-" + i);
@@ -32,6 +32,6 @@ public class Producer {
                     System.out.println("Msg sent: " + " key: " + record.key() +  " value: " + record.value() + " partition: " + partition + " offset: " + offset);
                 }
             }
-//        }
+        }
     }
 }

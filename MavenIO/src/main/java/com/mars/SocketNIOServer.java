d168f57b7e92417f51814c379a3fe3ce74dde917
Mars Shi <mars.shi@augmentum.com.cn>
2021-02-04 09:16:08 +0800

    remove

--- MavenIO/src/main/java/com/mars/SocketNIOServer.java
+++ MavenIO/src/main/java/com/mars/SocketNIOServer.java
@@ -1,5 +1,5 @@
 package com.mars;
 
 public class SocketNIOServer {
-    
+
 }
--- SpringCloudLearn/ms-movie/src/main/java/com/mars/User.java
+++
@@ -1,9 +0,0 @@
-package com.mars;
-
-import lombok.Data;
-
-@Data
-public class User {
-    private long id;
-    private String name;
-}
